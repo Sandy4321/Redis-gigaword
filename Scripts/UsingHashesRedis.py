18fe5af66b9af05cb76955683c9a35a66035b19e
rohitdholakia <rohit.dholakia@gmail.com>
2014-03-02 15:08:13 -0800

    Update UsingHashesRedis.py
    Also updated this script with 'with'. The code earlier was hideous if PEP-8 is applied. Sorry, Python!

--- Scripts/UsingHashesRedis.py
+++ Scripts/UsingHashesRedis.py
@@ -6,22 +6,25 @@ import redis
 def h(string):
 	return hash(string)
 
-r = redis.Redis(port=6479,host='localhost',db=0)
+r = redis.Redis(port = 6479, host = 'localhost', db=0)
 pipe = r.pipeline()
 count = 0
 MAX = 10000
-for line in gzip.open(sys.argv[1],'rb'):
-	if(len(line.split('\t')) < 2):
-		continue
-	parts = line.split('\t')
-	key = parts[1].rstrip('\n')
-	value = parts[0]
-	bucket = h(key) % 82000
-	if(count < MAX):
-		count += 1
-		pipe.hset(bucket,key,value)
-	else:
-		count = 0
-		pipe.hset(bucket,key,value)
-		pipe.execute()
+with os.popen('zcat ' + sys.argv[1]) as arpa_file:
+	for line in arpa_file:
+		
+
+		if len(line.split('\t')) < 2:
+			continue
+		parts = line.split('\t')
+		key = parts[1].rstrip('\n')
+		value = parts[0]
+		bucket = h(key) % 82000
+		if(count < MAX):
+			count += 1
+			pipe.hset(bucket,key,value)
+		else:
+			count = 0
+			pipe.hset(bucket,key,value)
+			pipe.execute()
 
