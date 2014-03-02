d30554ec358d7459856fbfc45862bf2994bc9582
rohitdholakia <rohit.dholakia@gmail.com>
2014-03-02 14:06:52 -0800

    Update WritingCorpus.py
    Updated using 'with' and os.popen for gzip

--- Scripts/WritingCorpus.py
+++ Scripts/WritingCorpus.py
@@ -14,23 +14,25 @@ if(len(sys.argv)<2):
 	sys.exit()
 
 startTime = time.time() 
-r = redis.Redis(port=6279,host='localhost',db=0)
-pipe = r.pipeline(transaction=True)
+r = redis.Redis(port = 6279, host = 'localhost', db = 0)
+pipe = r.pipeline(transaction = True)
 i = 0
 MAX = 10000
-for line in gzip.open(sys.argv[1],'rb'):
-	if('\\' not in line and '=' not in line):#this is only when using the ARPA file. 
-		try:	
-			parts = line.split('\t')
-			key = parts[1].rstrip('\n')
-			value = parts[0]
-			pipe.set(key, value)
-			if(i<MAX):
-				i=i+1
-			else:
-				i=0
-				pipe.execute()
-				i=i+1
-		except:
-			print line
+with os.popen('zcat ' + sys.argv[1]) as arpa_file:
+	#this is done because gzip files are slowly read when using gzip.open(). this one is fastest, relatively
+	for line in arpa_file:
+		if('\\' not in line and '=' not in line):#this is only when using the ARPA file. 
+			try:	
+				parts = line.split('\t')
+				key = parts[1].rstrip('\n')
+				value = parts[0]
+				pipe.set(key, value)
+				if(i<MAX):
+					i=i+1
+				else:
+					i=0
+					pipe.execute()
+					i=i+1
+			except:
+				print line
 print time.time() - startTime, 'seconds '
